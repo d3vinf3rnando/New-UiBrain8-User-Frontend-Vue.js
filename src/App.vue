93e89614040f90ed0381235ca26e99ab471f722e
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-12-23 10:31:12 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+<div>
+  <div class="flex justify-between gap-x-[7rem]">
+    <div class="order-first">UiBrain8</div>
+    <div class="order-last">0000</div>
+    
+</div>
+</div>
+</template>
+
+
+
+
